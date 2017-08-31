2e4f1199ba4bad430eb81e50e5fe0186cd6726a9
kripton <kamnev.u1969@gmail.com>
2017-08-31 08:42:49 +0300

    Reduced memory and added discounted rewards. However it seems to be non-efficient upgrade

--- environment.py
+++ environment.py
@@ -8,34 +8,60 @@ from Threaded import Threaded
 from time import sleep
 
 
-EPISODES = 5000
+EPISODES = 10000
 
 STATE_SIZE = 4
 ACTION_SPACE_SIZE = 2
-MEMORY_SIZE = 2000
+MEMORY_SIZE = 512
+
+
+def discounted_rewards(r, gamma=0.99):
+    discounted_r = np.zeros_like(r)
+    running_add = 0
+    for t in reversed(xrange(0, len(r))):
+        running_add = running_add * gamma + r[t]
+        discounted_r[t] = running_add
+
+    return discounted_r
+
+
+def episode_rerewarding(samples, rewards):
+    discounted_r = discounted_rewards(rewards)
+    for i in range(len(samples)):
+        samples[i][3] = discounted_r[i]
+
+    return samples
 
 
 def fill_memory(env, memory):
     s = env.reset()
+    samples = []
+    rewards = []
+    printed = False
 
     while memory.capacity != memory.size():
         a = env.action_space.sample()
         s1, r, d, _ = env.step(a)
 
-        sample = (s, s1, a, r)
-        memory.memorize(sample)
+        samples.append([s, s1, a, r])
+        rewards.append(r)
 
         s = s1
 
         if d:
+            if not printed:
+                print(samples)
+            samples = episode_rerewarding(samples, rewards)
+            if not printed:
+                print(samples)
+                printed = True
+
+            for sample in samples:
+                memory.memorize(sample)
+
             env.reset()
 
     return memory
-
-
-def reshape_state(s):
-    s1 = np.reshape(np.array(s), (4,))
-    return s1
 
 
 @Threaded.infinite("RenderThread")
@@ -47,7 +73,7 @@ def renderer(env):
 if __name__ == "__main__":
     env = gym.make("CartPole-v1")
 
-    hidden_sizes = [8, 4]
+    hidden_sizes = [16, 8]
     net = KerasQNet()
     net = net.build_net(STATE_SIZE, hidden_sizes, ACTION_SPACE_SIZE)
     memory = SimpleMemory(MEMORY_SIZE)
@@ -60,24 +86,39 @@ if __name__ == "__main__":
         env.reset()
         renderer(env)
 
+        rewards = []
+
         for i in range(EPISODES):
-            s = reshape_state(env.reset())
+            s = env.reset()
             done = False
+
             episode_reward = 0
+            samples = []
+            episode_rewards = []
 
             while not done:
                 a = agent.act(s)
                 s1, r, done, _ = env.step(a)
-                s1 = reshape_state(s1)
 
-                sample = (s, s1, a, r)
-                agent.memorize(sample)
                 agent.replay()
 
-                episode_reward += 1
+                episode_rewards.append(r)
+                samples.append([s, s1, a, r])
 
-            print("Episode %d reward: %f" % (i, episode_reward))
+                episode_reward += r
+
+            samples = episode_rerewarding(samples, episode_rewards)
+            for sample in samples:
+                memory.memorize(sample)
+
+            # Save for some statistics
+            rewards.append(episode_reward)
+
+            if i % 100 == 0 and i != 0:
+                print("Passed %d episodes" % i)
+                print("Mean reward for last 100: %f" %
+                      (np.mean(rewards[-100:])))
 
     finally:
-        net.save("keras-qmodel0.h5")
+        net.save("models/keras-qmodel0.h5")
         Threaded.stop_thread("RenderThread")
