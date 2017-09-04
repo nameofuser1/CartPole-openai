fbd29220b6c6c29f8328a6ddad3168f698ea60a9
kripton <kamnev.u1969@gmail.com>
2017-09-04 17:51:39 +0300

    Memorizing considering prioritized memory

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 import numpy as np
+import logging
 
 
 class CartpoleAgent(object):
@@ -18,6 +19,13 @@ class CartpoleAgent(object):
         self._min_explore_rate = min_explore_rate
         self._gamma = gamma
 
+        fhandler = logging.FileHandler("logs/agent.log", 'w')
+        fhandler.setLevel(logging.DEBUG)
+
+        self._logger = logging.getLogger()
+        self._logger.setLevel(logging.DEBUG)
+        self._logger.addHandler(fhandler)
+
     def act(self, s):
         if np.random.rand() <= self._explore_rate:
             action = self._action_space.sample()
@@ -26,10 +34,25 @@ class CartpoleAgent(object):
 
         return action
 
-    def memorize(self, s):
-        self._memory.memorize(s)
+    def memorize(self, sample):
+        s = sample[0]
+        s1 = sample[1]
+        a = sample[2]
+        r = sample[3]
+
+        q_target = self._net.predict(s)
+        action_q = r + self._gamma*max(self._net.predict(s1))
+        dq = np.abs(q_target[a] - action_q)
+
+        q_target[a] = action_q
+
+        # Memorize with dq as priority weight
+        item = (dq, (q_target, sample))
+        self._memory.memorize(item)
 
     def replay(self, batch_size=32):
+        """
+        """
         batch = self._memory.remember(batch_size)
         x, y = self.__get_targets(batch)
         hist = self._net.train(x, y, batch_size=batch_size)
@@ -40,6 +63,9 @@ class CartpoleAgent(object):
         return np.mean(hist.history['loss'])
 
     def __get_targets(self, batch):
+        """
+        batch = [(weight0, (q0, sample0)), (weight1, (q1, sample1)), ...]
+        """
         x = np.zeros((len(batch), self._state_size),
                      dtype=np.float32)
         y = np.zeros((len(batch), self._action_space_size),
@@ -48,11 +74,11 @@ class CartpoleAgent(object):
         k = 0
         put_xrange = range(self._state_size)
         put_yrange = range(self._action_space_size)
-        for s, s1, a, r, d, steps in batch:
-            q_target = self._net.predict(s)
-            action_q = r + self._gamma*max(self._net.predict(s1))
 
-            q_target[a] = action_q
+        for weight, item in batch:
+            q_target, sample = item
+            s, s1, a, r, d, steps = sample
+
             np.put(x[k], put_xrange, s)
             np.put(y[k], put_yrange, q_target)
 
